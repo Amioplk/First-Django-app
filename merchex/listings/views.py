f7b6cfed7f2a0a7d92310570a2ce88c48be902d3
Amioplk <amir.worms@dauphine.eu>
2023-10-05 15:49:33 +0200

    New url models

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,15 @@
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello World!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
+def listings(request):
+    return HttpResponse('<h1>Liste des annonces</h1>')
+
+def contact_us(request):
+    return HttpResponse('<h1></h1><p>Contactez-nous à l\'adresse suivante : <a href="mailto:amirworms@gmail.com">Mail de contact</a><p>')
